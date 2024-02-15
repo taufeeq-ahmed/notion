32b4c0935710df29d3a9f484daa570cce90549ab
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-02-15 15:02:36 +0530

    migration setup done

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,20 +1,19 @@
-import type,{Config} from 'drizzle-kit'
+import type, { Config } from 'drizzle-kit'
 import * as dotenv from 'dotenv'
-
+import { error } from 'console'
 dotenv.config({
-    path:'.env'
+    path: '.env'
 })
 
-if(!process.env.DATABASE_URL){
-    console.log('🔴 cannot find database url')
+if (!process.env.DATABASE_URL) {
+    error('🔴 cannot find database url')
 }
 
 export default {
-    schema:'./src/lib/supabase/schema.ts',
-    out:'./migrations',
-    driver:'pg',
-    dbCredentials :{
-        connectionString : process.env.DATABASE_URL || ''
+    schema: './src/lib/supabase/schema.ts',
+    out: './migrations',
+    driver: 'pg',
+    dbCredentials: {
+        connectionString: process.env.DATABASE_URL || ''
     }
 } satisfies Config
-
---
+++ src/app/lib/supabase/db.ts
@@ -0,0 +1,31 @@
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import * as dotenv from 'dotenv'
+import * as schema from '../../../../migrations/schema'
+import { log, error } from 'console'
+import { migrate } from 'drizzle-orm/postgres-js/migrator'
+
+dotenv.config({
+    path: '.env'
+})
+
+if (!process.env.DATABASE_URL) {
+    log('🔴 cannot find database url')
+}
+
+const client = postgres(process.env.DATABASE_URL as string, { max: 1 })
+
+const db = drizzle(client, { schema })
+
+const migrateDB = async () => {
+    try {
+        log('🟠 migrating client')
+        await migrate(db, { migrationsFolder: 'migrations' })
+        log('🟢 successfully migrated !!')
+    } catch (err) {
+        error('🔴 error migrating client', err)
+    }
+}
+migrateDB()
+
+export default db
