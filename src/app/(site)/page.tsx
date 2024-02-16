2a7329c3519fda2545128ab8a97b23cbcfdc93a9
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-02-16 16:22:26 +0530

    app banner added

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,8 @@
 import TitleSection from '@/components/LandingPage/TitleSection'
+import { Button } from '@/components/ui/Button'
+import Image from 'next/image'
 import React from 'react'
+import appBanner from './../../../public/appBanner.png'
 
 function HomePage() {
     return (
@@ -11,6 +14,55 @@ function HomePage() {
                     pill="✨ Your Workspace, Perfected"
                     title="All-In-One Collaboration and Productivity Platform"
                 />
+                <div
+                    className="bg-white
+                    p-[2px]
+                    mt-6
+                    rounded-xl
+                    bg-gradient-to-r
+                    from-primary
+                    to-brand-primaryBlue
+                    sm:w-[300px]
+        "
+                >
+                    <Button
+                        variant="btn-secondary"
+                        className=" w-full
+                        rounded-[10px]
+                        p-6
+                        text-2xl
+                        bg-background"
+                    >
+                        Get Notion for Free
+                    </Button>
+                </div>
+                <div
+                    className="md:mt-[-90px]
+                    sm:w-full
+                    w-[750px]
+                    flex
+                    justify-center
+                    items-center
+                    mt-[-40px]
+                    relative
+                    sm:ml-0
+                    ml-[-50px]"
+                >
+                    <Image
+                        src={appBanner}
+                        alt="Application Banner"
+                    />
+                    <div
+                        className="bottom-0
+                        top-[50%]
+                        bg-gradient-to-t
+                        dark:from-background
+                        left-0
+                        right-0
+                        absolute
+                        z-10"
+                    ></div>
+                </div>
             </div>
         </section>
     )
